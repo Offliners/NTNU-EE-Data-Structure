b614cd33ec941111a0eba05803c6828339cc9306
Offliner <42777325+Offliners@users.noreply.github.com>
2019-09-23 16:52:26 +0800

    Create Selection-sort-algorithm.c

---
+++ Week-2/Selection-sort-algorithm.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+#define MAXSIZE 101
+
+void Sort(int [],int);
+void Swap(int *,int *);
+
+void main(void)
+{
+	int list[MAXSIZE];
+	int n,i;
+
+	printf("Enter the number of integers : ");
+	scanf("%d",&n);
+	if(n < 1 || n > MAXSIZE)
+	{
+		fprintf(stderr,"Error input!\n");
+		exit(EXIT_FAILURE);
+	}
+	for(i = 0;i < n;i++)
+	{
+		list[i] = rand() % 1000;
+		printf("%d ",list[i]);
+	}
+	printf("\nSorted Array :\n");
+	Sort(list,n);
+	for(i = 0;i < n;i++)
+		printf("%d ",list[i]);
+}
+
+void Sort(int list[],int n)
+{
+	int i,j,min,temp;
+
+	for(i = 0;i < n;i++)
+	{
+		min = i;
+
+		for(j = i + 1;j < n;j++)
+		{
+			if(list[j] < list[min])
+				min = j;
+		}
+		Swap(&list[i],&list[min]);
+	}
+}
+
+void Swap(int *a,int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
