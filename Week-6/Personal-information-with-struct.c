d8c7750f9a6af9f2ca65bff7c191f9bfa7e9a9fd
Offliner <42777325+Offliners@users.noreply.github.com>
2019-10-14 16:48:05 +0800

    Update Personal-information-with-struct.c

--- Week-6/Personal-information-with-struct.c
+++ Week-6/Personal-information-with-struct.c
@@ -1,1 +1,87 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
+#define FALSE 0
+#define TRUE 1
+
+struct data{
+	int month;
+	int day;
+	int year;
+};
+
+struct human_being{
+	char name[10];
+	int age;
+	float salary;
+	struct data dob;
+};
+
+typedef struct{
+	int sex;
+	union{
+		int children;
+		int beard;
+	}u;
+}sex_type;
+
+int HumanEqual(struct human_being person1,struct human_being person2);
+
+void main(void)
+{
+	struct human_being person1,person2;
+
+	strcpy(person1.name,"James");
+	person1.age = 18;
+	person1.salary = 30000;
+	person1.dob.month = 2;
+	person1.dob.day = 11;
+	person1.dob.year = 2001;
+
+	strcpy(person2.name,"Jos");
+	person1.age = 18;
+	person1.salary = 30000;
+	person1.dob.month = 3;
+	person1.dob.day = 18;
+	person1.dob.year = 1999;
+
+	if(HumanEqual(person1,person2))
+		printf("The same!\n");
+	else
+		printf("Not the same!\n");
+
+	sex_type person3;
+	person3.sex = 1;
+	person3.u.beard = 0;
+	person3.u.children = 1;
+
+	printf("Setting of beard : %d\n",person3.u.beard);
+	if(person3.sex == 0)
+	{
+		printf("Female\n");
+		if(person3.u.children == 1)
+			printf("Girl\n");
+		else
+			printf("Woman\n");
+	}
+	else
+	{
+		printf("Male\n");
+		if(person3.u.children == 1)
+			printf("Boy\n");
+		else
+			printf("Man\n");
+	}
+}
+
+int HumanEqual(struct human_being person1,struct human_being person2)
+{
+	if(strcmp(person1.name,person2.name))
+		return FALSE;
+	if(person1.age != person2.age)
+		return FALSE;
+	if(person1.salary != person2.salary)
+		return FALSE;
+	return TRUE;
+}
