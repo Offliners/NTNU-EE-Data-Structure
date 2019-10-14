5b0a55ad863d523520c1419e4ac09e100a5d3eb4
Offliner <42777325+Offliners@users.noreply.github.com>
2019-10-14 11:22:32 +0800

    Update More-accurate-timing-program-for-selection-sort.c

--- Week-6/More-accurate-timing-program-for-selection-sort.c
+++ Week-6/More-accurate-timing-program-for-selection-sort.c
@@ -6,11 +6,11 @@
 
 void main(void)
 {
-	int i,n,step = 10;
-	int a[MAXSIZE];
-	double duration;
-	clock_t start;
-	long repetitions;
+    int i,n,step = 10;
+    int a[MAXSIZE];
+    double duration;
+    clock_t start;
+    long repetitions;
 
     printf("   n      repetitions      time\n");
     for(n = 0;n <=1000;n += step)
@@ -24,10 +24,10 @@ void main(void)
     			a[i] = n - i;
     		sort(a,n);
     	}while(clock() - start < 1000);
-	    duration = (double)(clock() - start) / CLOCKS_PER_SEC;
-	    duration /= repetitions;
-	    printf("%4d   %14d    %f\n",n,repetitions,duration);
-	    if(n == 100)
-	    	step = 100;
+        duration = (double)(clock() - start) / CLOCKS_PER_SEC;
+        duration /= repetitions;
+        printf("%4d   %14d    %f\n",n,repetitions,duration);
+        if(n == 100)
+       	    step = 100;
 	}
 }
