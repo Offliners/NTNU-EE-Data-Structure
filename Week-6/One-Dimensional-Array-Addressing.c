1e96a135725b6da4c40c1bf2b72467822ac8e04d
Offliner <42777325+Offliners@users.noreply.github.com>
2019-10-14 11:47:01 +0800

    Update One-Dimensional-Array-Addressing.c

--- Week-6/One-Dimensional-Array-Addressing.c
+++ Week-6/One-Dimensional-Array-Addressing.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+void print1(int *ptr,int rows);
+
+void main(void)
+{
+	int one[] = {1,2,3,4,5};
+
+	print1(one,5);
+}
+
+void print1(int *ptr,int rows)
+{
+	int i;
+
+	printf("Address Contents\n");
+	for(i = 0;i < rows;i++)
+		printf("%8u%5d\n",ptr+i,*(ptr + i));
+	    // printf("%8u%5d\n", &ptr[i], ptr[i]);
+	printf("\n");
+}
