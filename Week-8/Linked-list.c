e327b4d5ef948818d8365dff6509980573f52d4e
Offliner <42777325+Offliners@users.noreply.github.com>
2019-10-28 09:54:51 +0800

    Create Linked-list.c

---
+++ Week-8/Linked-list.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node
+{
+	char data;
+	struct node *link;
+}list;
+
+void main()
+{
+	list item1,item2,item3,i;
+
+	item1.data = 'a';
+	item2.data = 'b';
+	item3.data = 'c';
+	item1.link = &item2;
+	item2.link = &item3;
+	item3.link = NULL;
+	for(i = item1;i.link != NULL;i = *(i.link))
+	{
+		printf("%c ",i.data);
+		if((*(i.link)).link == NULL)
+			printf("%c ",(*(i.link)).data);
+	}
+}
