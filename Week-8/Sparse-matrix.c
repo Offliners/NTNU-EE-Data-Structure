a6956147df048e422ebc2ba4a38ce13bb8ea34a5
Offliner <42777325+Offliners@users.noreply.github.com>
2019-11-11 01:11:43 +0800

    Update Sparse-matrix.c

--- Week-8/Sparse-matrix.c
+++ Week-8/Sparse-matrix.c
@@ -66,18 +66,18 @@ void transpose(term a[],term b[])
 	n = a[0].value;
 	b[0].row = a[0].col;    
 	b[0].col = a[0].row;    
-    b[0].value = n;         
-    if(n > 0) 
+    	b[0].value = n;         
+    	if(n > 0) 
 	{   
-    	currentb = 1;
-        for(i = 0; i < a[0].col; i++)
+    		currentb = 1;
+       		for(i = 0; i < a[0].col; i++)
         	for(j = 1; j <= n; j++)
-            	if (a[j].col == i) 
-				{
-                	b[currentb].row = a[j].col;
-                    b[currentb].col = a[j].row;
-                    b[currentb].value = a[j].value;
-                    currentb++;
-                } 
-    }
+            		if (a[j].col == i) 
+			{
+                		b[currentb].row = a[j].col;
+                    		b[currentb].col = a[j].row;
+                    		b[currentb].value = a[j].value;
+                    		currentb++;
+                	} 
+   	 }
 }
