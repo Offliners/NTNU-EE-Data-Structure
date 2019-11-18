0ed30ce2d67cc3381d387fc1e8e027ec83d8c1ef
Offliner <42777325+Offliners@users.noreply.github.com>
2019-11-18 09:57:13 +0800

    Update Sparse-matrix.c

--- Week-8/Sparse-matrix.c
+++ Week-8/Sparse-matrix.c
@@ -10,11 +10,9 @@ typedef struct
 	int value;
 }term;
 
-void transpose(term a[],term b[]);
-
 void main()
 {
-	term a[MAXTERM],b[MAXTERM];
+	term a[MAXTERM];
 	int i;
 
 	a[0].row = 6;
@@ -50,34 +48,4 @@ void main()
 		printf("(%d, %d, %d)",a[i].row,a[i].col,a[i].value);
 		printf("\n");
 	}
-	transpose(a,b);
-	printf("Output b:\n");
-	for(i = 0;i <= 8;i++)
-	{
-		printf("(%d, %d, %d)",b[i].row,b[i].col,b[i].value);
-		printf("\n");
-	}
-}
-
-void transpose(term a[],term b[])
-{
-	int n,i,j,currentb;
-
-	n = a[0].value;
-	b[0].row = a[0].col;    
-	b[0].col = a[0].row;    
-    	b[0].value = n;         
-    	if(n > 0) 
-	{   
-    		currentb = 1;
-       		for(i = 0; i < a[0].col; i++)
-        	for(j = 1; j <= n; j++)
-            		if (a[j].col == i) 
-			{
-                		b[currentb].row = a[j].col;
-                    		b[currentb].col = a[j].row;
-                    		b[currentb].value = a[j].value;
-                    		currentb++;
-                	} 
-   	 }
 }
